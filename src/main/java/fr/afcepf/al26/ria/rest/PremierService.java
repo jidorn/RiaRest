8346690a809e5b4797cd6d99ad1d81843121c502
Alexandre QUERE <alexquere1@gmail.com>
2016-03-18 11:20:35 +0100

    ajout des params en GET

--- src/main/java/fr/afcepf/al26/ria/rest/PremierService.java
+++ src/main/java/fr/afcepf/al26/ria/rest/PremierService.java
@@ -2,6 +2,7 @@ package fr.afcepf.al26.ria.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -20,5 +21,23 @@ public class PremierService {
     @Produces(MediaType.TEXT_PLAIN)
     public String helloWorld(){
         return "Rest In Peace";
+    }
+
+    @GET
+    @Path("/hello/{p1}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String testParam(
+            @PathParam(value = "p1") String param1){
+        return "Rest In Peace"+ param1;
+    }
+
+    @GET
+    @Path("/hello/{p1}/{p2}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String testParam(
+            @PathParam(value = "p1") String param1,
+            @PathParam(value = "p2") String param2
+    ){
+        return "Rest In Peace : "+ param1+", "+param2;
     }
 }
